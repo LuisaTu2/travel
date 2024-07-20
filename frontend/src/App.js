4d013b0c8518b67a7ea17256e5d7272cbc78695d
luisapegoraro <luisa.pegoraro@gmail.com>
2024-07-19 21:20:25 -0400

    [isa] added architecture and link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 import Photo from "./Photo.js";
-import { GET_PHOTOS_BEOGRAD } from "./constants.js";
+import { GET_PHOTOS_BEOGRAD, ARCHITECTURE_PATH } from "./constants.js";
 
 function App() {
   const [photos, setPhotos] = useState([]);
@@ -28,6 +28,8 @@ function App() {
             return <Photo data={photo} key={photo.title} />;
           })}
           <div> with love {phrase} </div>
+          <div>Curious about the implementation details? </div> Check out the <strong><a href={ARCHITECTURE_PATH} style={{color: "white", textDecoration: "none"}}> architecture</a></strong> page.
+          <footer>made with &#129293; in &#127809; </footer>
         </ul>
       </header>
     </div>
